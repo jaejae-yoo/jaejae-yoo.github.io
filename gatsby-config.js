66b2541cbdbf12bdbfe1828239d9e327604fa3d0
wotj102@gmail.com <wotj102@gmail.com>
2021-01-22 13:45:56 +0900

    til

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ const shouldAnalyseBundle = process.env.ANALYSE_BUNDLE
 
 module.exports = {
   siteMetadata: {
-    siteTitle:'TIL',
+    siteTitle:'Jaeseo_TIL',
     siteTitleAlt: `Jaeseo`,
   },
   plugins: [
